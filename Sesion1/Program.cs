81e8100e94e469cdf20776dbfb0e7cbfdf8b7011
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-07-23 23:08:56 -0500

    Ejercicios Sesión 1

---
+++ Sesion1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Ingrese su nombre:");
+string name = Console.ReadLine();
+Console.WriteLine("Buenos días " + name);
+
+string time = DateTime.Now.ToString("h:mm:ss tt");
+Console.WriteLine("La hora actual: " + time);
